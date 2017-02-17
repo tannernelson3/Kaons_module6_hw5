da81d777bc4806992ac797445b85b688a1bc9a34
Tanner Nelson <tannernelson3@mail.weber.edu>
2017-02-17 16:39:32 +0000

    Initial Commit

--- Kaons_hw5_task1.c
+++ Kaons_hw5_task1.c
@@ -1,7 +1,28 @@
 #include <stdio.h>
 
+double AnnulusMoment(double outr, double inr);
+
 int main(void)
 {
+	double outr, inr;
 
- return 0;
+	printf("Enter the value for the outer radius (r2): ");
+	scanf("%lf", &outr);
+	printf("Enter the value for the inner radius (r1): ");
+	scanf("%lf", &inr);
+	
+	AnnulusMoment(outr, inr);
+
+
+	return 0;
+}
+
+double AnnulusMoment(double outr,double inr)
+{
+	double I;
+	I = (3.14159265359/4)*((outr*outr*outr*outr)-(inr*inr*inr*inr));
+	printf("The second moment of inertia is %5.2f\n", I);
+
+	return I;
+	
 }
