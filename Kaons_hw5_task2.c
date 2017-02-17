7a1802a79daae07dca1ca83a515e48b67f701b63
Tanner Nelson <tannernelson3@mail.weber.edu>
2017-02-17 17:38:08 +0000

    Initial Commit

--- Kaons_hw5_task2.c
+++ Kaons_hw5_task2.c
@@ -1,7 +1,37 @@
 #include <stdio.h>
 
+double MakeMilesKmTable(int startmiles, int endmiles, int inc);
+
 int main(void)
 {
+	int startmiles, endmiles;
+	int inc;
+	
+	printf("Enter the starting miles: ");
+	scanf("%d", &startmiles);
+	printf("Enter the ending miles: ");
+	scanf("%d", &endmiles);
+	printf("Enter the increments: ");
+	scanf("%d", &inc);
+
+
+
+	MakeMilesKmTable(startmiles,endmiles,inc);
 
 return 0;
+}
+
+double MakeMilesKmTable(int startmiles, int endmiles, int inc)
+{
+
+	double k;
+	int x;
+	printf("Miles    =    Kilometers\n");
+	for(x = startmiles; x <= endmiles; x += inc)
+	{
+		k = x * 1.61;
+		printf(" %d		%5.2f\n", x, k);
+	}
+
+return k;
 }
